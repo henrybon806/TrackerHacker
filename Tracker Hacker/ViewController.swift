c8af544b60480c87b91d89c3eb91da50a27bad51
Henry Bonomolo <henrybon806@gmail.com>
2021-11-26 16:47:22 -0700

    Initial Commit

---
+++ Tracker Hacker/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Tracker Hacker
+//
+//  Created by Henry Bonomolo on 11/19/21.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var questionmark: UIButton!
+    @IBOutlet weak var begin: UIButton!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    fileprivate let locationManager:CLLocationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        background.layer.cornerRadius = 25
+        questionmark.layer.cornerRadius = 10
+        questionmark.tintColor = UIColor.lightGray
+        begin.layer.cornerRadius = 10
+        begin.tintColor = UIColor.systemGreen
+        begin.backgroundColor = UIColor.systemGreen
+        begin.isEnabled = false
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        
+        map.showsUserLocation = true
+        map.layer.zPosition = -1
+    }
+
+    
+}
---
+++ Tracker HackerUITests/Tracker_HackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tracker_HackerUITestsLaunchTests.swift
+//  Tracker HackerUITests
+//
+//  Created by Henry Bonomolo on 11/19/21.
+//
+
+import XCTest
+
+class Tracker_HackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
